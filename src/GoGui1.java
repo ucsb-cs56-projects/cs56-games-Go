f2ad7bd64c8a3c5aa0da85c9517a969c8d76b755
dong1016 <donghe2006@gmail.com>
2016-11-07 15:43:16 -0800

    issue #42 done
    Cleaned up the code and removed old code that was no longer used and
    reformatted code that is being used

--- src/GoGui1.java
+++ src/GoGui1.java
@@ -1,4 +1,3 @@
-// package edu.ucsb.cs56.S12.dwinkenwerder.lab06;
 import javax.swing.JFrame;
 import java.awt.ComponentOrientation;
 import javax.swing.BoxLayout;
@@ -11,72 +10,67 @@ import java.awt.Font;
 import java.awt.Color;
 
 /** GoGui01.java is a GUI interface for TicTacToe that uses
-    the JTextArea as the destination for messages.
-
-     @author David Winkenwerder and Dustin Henderson
-     @author Keith Waldron and Nick Abrahan
-     @version CS56, Winter 2014, UCSB
-*/
+ the JTextArea as the destination for messages.
+ 
+ @author David Winkenwerder and Dustin Henderson
+ @author Keith Waldron and Nick Abrahan
+ @version CS56, Winter 2014, UCSB
+ */
 
 public class GoGui1 {
-
-    /** main method to fire up a JFrame on the screen  
-          @param args not used
-    */
-
+    
+    /** main method to fire up a JFrame on the screen
+     @param args not used
+     */
+    
     public static void main (String[] args) {
-	JFrame frame = new JFrame() ;
-	frame. setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE) ;
-	char winner=' ';
-	boolean surrender1=false;boolean surrender2=false;
-
-
-	// since GoGrid implements the GoGame interface,
-	// we can use GoGame on left hand side.
-	frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(),0));
-	GoGame game = new GoGrid();
+        JFrame frame = new JFrame() ;
+        frame. setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE) ;
+        char winner=' ';
+        boolean surrender1=false;boolean surrender2=false;
+        
+        
+        // since GoGrid implements the GoGame interface,
+        // we can use GoGame on left hand side.
+        frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(),0));
+        GoGame game = new GoGrid();
         JTextArea  md = new JTextArea(4,4);
-	GoComponent gc = new GoComponent(game, md);	
-	ButtonAndScrollComponent basc = new ButtonAndScrollComponent(game, md, gc);
-	
-	gc.setPreferredSize(new Dimension(500,500));
-	basc.setPreferredSize(new Dimension(300,150));
-	Font f = new Font("Verdana", Font.BOLD, 15);
-	md.setFont(f);
-	Color gray = new Color(213,216,220);
-	md.setForeground(gray);
-
-	//basc.setOpaque(false);
-	Color grayb = new Color(40,55,71);
-	basc.getmd().setBackground(grayb);
-	
-	
-	frame.getContentPane().add(gc);
-	frame.getContentPane().add(basc);
-	// to make sure that grids go left to right, no matter what
-
-	frame .applyComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-	frame. setResizable(false);
-	frame. setSize(800,650) ;
-	frame. setVisible(true) ;
-
-
-	//so message destination automatically scrolls to the bottom as more is added 
-	try {
-               	md.setCaretPosition(md.getLineStartOffset(md.getLineCount() - 1));
-            } catch (BadLocationException e) {
-                e.printStackTrace();
-	    }
-
-
-
-
-	if(game.getSurrender1() && game.getSurrender2()){
-	    game.setSurrendering();
-            //winner=game.getWinner();
-
-	  
-	}
-
+        GoComponent gc = new GoComponent(game, md);
+        ButtonAndScrollComponent basc = new ButtonAndScrollComponent(game, md, gc);
+        
+        gc.setPreferredSize(new Dimension(500,500));
+        basc.setPreferredSize(new Dimension(300,150));
+        Font f = new Font("Verdana", Font.BOLD, 15);
+        md.setFont(f);
+        Color gray = new Color(213,216,220);
+        md.setForeground(gray);
+        
+        Color grayb = new Color(40,55,71);
+        basc.getmd().setBackground(grayb);
+        
+        
+        frame.getContentPane().add(gc);
+        frame.getContentPane().add(basc);
+        // to make sure that grids go left to right, no matter what
+        
+        frame .applyComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        frame. setResizable(false);
+        frame. setSize(800,650) ;
+        frame. setVisible(true) ;
+        
+        
+        //so message destination automatically scrolls to the bottom as more is added
+        try {
+            md.setCaretPosition(md.getLineStartOffset(md.getLineCount() - 1));
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+        
+        
+        if(game.getSurrender1() && game.getSurrender2()){
+            game.setSurrendering();
+            
+        }
+        
     }
 }
